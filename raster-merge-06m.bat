7fa1170f28d161004258d360a2e32bcdadf80e6e
Jakub Szostak <jszostak@UMGDY.gov.pl>
2018-09-03 08:13:07 +0200

    change merge-60 from gdalwrap to gdal_translate

--- raster-merge-06m.bat
+++ raster-merge-06m.bat
@@ -4,17 +4,16 @@ echo *** %1
 echo.
 call %~dp0\_init.bat %1 %2 %3
 
-SET PixelSize=-tr 0.6 0.6
 
-:: -srcnodata 0.0 -dstnodata -9999.0
-SET NoData=-srcnodata 255 -dstnodata 0
+SET MergeName=_merge-60cm
+SET VrtParams=-vrtnodata 255 
+SET PixelSize=-tr 0.6 0.6 -r average
 
- 
-gdalwarp %PixelSize% %NoData% -r average -wm %GDAL_CACHEMAX% -ovr NONE %TiffOpts% *%SrcExt% _merge-60cm.tif
+gdalbuildvrt %VrtParams% %MergeName%.vrt *%SrcExt%
+gdal_translate %TiffOpts% %PixelSize% %MergeName%.vrt %MergeName%.tif
 
-
-%~dp0\raster-pyramid.bat _merge-60cm.tif
-:: %~dp0\raster-calc-stats.bat _merge-60cm.tif
+call %~dp0\raster-pyramid.bat %MergeName%.tif
+:: %~dp0\raster-calc-stats.bat %MergeName%.tif
 
 
 echo.
