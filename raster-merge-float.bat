f1e8afa66d5ace87e6438e7108dd35a693d76a04
kubaszostak <kuba.szostak@gmail.com>
2019-02-16 08:57:28 +0100

    move to direct gdal calls

--- raster-merge-float.bat
+++ raster-merge-float.bat
@@ -1,5 +1,12 @@
 @echo off
+
+call %~dp0\_init.bat %1 %2 %3
 SET MergeParams=-vrtnodata -9999.0 
 
-%~dp0\raster-merge.bat %1 
+gdalbuildvrt %MergeParams% _merge.vrt *%SrcExt%
+
+gdal_translate %TiffOpts%  _merge.vrt _merge.tif
+
+call %~dp0\raster-pyramid.bat _merge.tif
+
 %~dp0\raster-calc-stats.bat _merge.tif
